bd073aaaf7ab7ba994ba6c3f225365bf5d496814
Nine_Dev <piyapat@globalwireless.co.th>
2021-03-12 14:10:58 +0700

    battle

---
+++ Assets/Script/Gun.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public Bullet Bullet;
+    public ParticleSystem Explosion;
+    public Transform[] AllBulletOut;
+    public float firerate;
+    int BulletOutIndex;
+    Coroutine FireBehavior;
+    bool IsFire;
+    
+    void Start()
+    {
+        
+    }
+
+    public void StartFire()
+    {
+        IsFire = true;
+    }
+
+    public void EndFire()
+    {
+        IsFire = false;
+    }
+
+    IEnumerator Fire()
+    {
+        while (IsFire)
+        {
+            Bullet BC = Instantiate(Bullet);
+            BC.transform.eulerAngles = AllBulletOut[BulletOutIndex].eulerAngles;
+            BC.Shoot();
+            BulletOutIndex = BulletOutIndex == AllBulletOut.Length-1? BulletOutIndex+=1 : 0;
+            yield return new WaitForSeconds(firerate);
+        }
+    }
+}
